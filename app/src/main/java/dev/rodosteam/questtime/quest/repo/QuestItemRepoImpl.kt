f40bb93a98cedafdf76af29407fdc8f14e784b66
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2022-04-06 22:59:12 +0300

    Added Repository mock and simple quests-generator class

--- app/src/main/java/dev/rodosteam/questtime/quest/repo/QuestItemRepoImpl.kt
+++ app/src/main/java/dev/rodosteam/questtime/quest/repo/QuestItemRepoImpl.kt
@@ -2,20 +2,41 @@ package dev.rodosteam.questtime.quest.repo
 
 import dev.rodosteam.questtime.quest.model.QuestItem
 
-class QuestItemRepoImpl() : QuestItemRepo {
+class QuestItemRepoImpl : QuestItemRepo {
+    private val questsList = arrayListOf<QuestItem>()
+
+    init {
+        val questsInfo = QuestsInfo()
+        questsList.add(questsInfo.generateRandomQuest(1))
+        questsList.add(questsInfo.generateRandomQuest(2))
+        questsList.add(questsInfo.generateRandomQuest(3))
+        questsList.add(questsInfo.generateRandomQuest(4))
+        questsList.add(questsInfo.generateRandomQuest(5))
+    }
+
     override fun findAll(): List<QuestItem> {
-        TODO("Not yet implemented")
+        return questsList
     }
 
-    override fun findById(id: Long): QuestItem {
-        TODO("Not yet implemented")
+    override fun findById(id: Int): QuestItem? {
+        for (quest in questsList) {
+            if (quest.id == id) {
+                return quest
+            }
+        }
+        return null
     }
 
-    override fun findByName(name: String): QuestItem {
-        TODO("Not yet implemented")
+    override fun findByName(name: String): QuestItem? {
+        for (quest in questsList) {
+            if (quest.title == name) {
+                return quest
+            }
+        }
+        return null
     }
 
     override fun add(item: QuestItem): Boolean {
-        TODO("Not yet implemented")
+        return questsList.add(item)
     }
 }
