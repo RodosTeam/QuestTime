76f7293b4008fa4d9ab81d2c5c2e676028a26e72
levs2001 <levs.2001@gmail.com>
2022-04-07 13:45:45 +0300

    Start

--- app/src/main/java/dev/rodosteam/questtime/quest/model/QuestContent.kt
+++ app/src/main/java/dev/rodosteam/questtime/quest/model/QuestContent.kt
@@ -2,5 +2,7 @@ package dev.rodosteam.questtime.quest.model
 
 //TODO
 class QuestContent(val someTreeMap: Collection<Int>) {
-
+    // load (jsonname.str)
+    // getNewState(Ans)
+    // isEnd()
 }
